a970f0c361487cb6a02d7a80fc5cd4808c9d0a52
ddururiiiiiii <o244227@daum.net>
2024-01-15 10:03:27 +0900

    #004 - 타임라인 전체 조회 html, controller 생성

---
+++ src/main/java/toyproject/bookbookclub/web/timeline/TimelineController.java
@@ -0,0 +1,34 @@
+package toyproject.bookbookclub.web.timeline;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import toyproject.bookbookclub.domain.Timeline.TimeLineRepository;
+import toyproject.bookbookclub.domain.Timeline.Timeline;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/timeline")
+public class TimelineController {
+
+
+    private final TimeLineRepository timeLineRepository;
+
+    @GetMapping("/allTimeline")
+    public String allTimeline(Model model){
+        List<Timeline> timelines = timeLineRepository.findAll();
+        model.addAttribute("timelines", timelines);
+        return "timeline/allTimeline";
+    }
+
+    @PostConstruct
+    public void init(){
+        timeLineRepository.save(new Timeline("1", "bookId1", "bookImg1", "memberId1", "content1", "2024-01-15"));
+        timeLineRepository.save(new Timeline("2", "bookId2", "bookImg2", "memberId2", "content2", "2024-01-16"));
+    }
+}
