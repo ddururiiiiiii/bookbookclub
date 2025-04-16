8418af93c8f6d626cb354677d0f75517e813cd87
ddururiiiiiii <o244227@daum.net>
2025-04-16 17:54:56 +0900

    Initial commit

---
+++ src/test/java/ddururi/bookbookclub/BookbookclubApplicationTests.java
@@ -0,0 +1,13 @@
+package ddururi.bookbookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbookclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
